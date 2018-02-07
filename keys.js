abe506693d30ceb47a1d822ad25322ba7aa0a49a
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-02-06 20:17:17 -0800

    twitter added for respnse

---
+++ keys.js
@@ -0,0 +1,19 @@
+// console.log('keys are loaded');
+
+var twitterKeys = {
+    consumer_key: 'j8SQNUgxrEvfDFtppsnpGyisp',
+    consumer_secret: 'cFRaNn8lVr2vaIYDoiz5TwPQ77PZIhiQGy9JtxdztfWbpZiaA0',
+    access_token_key: '59694379-u5V5YdnhV2AF6YsaC5RtxrKDcfgjt1f9PMmNRaT4B',
+    access_token_secret: 'LGfZmixMr7POBNzMoLvBU2QYpbYV9zMAuRHPCY6F2nEqa',
+}
+
+
+var spotifyKeys = {
+    id: "5f027c20f59b4045b43efba0d3c118ea",
+    secret: "bb60f0c0a52944498a897314e58e1ba4"
+}
+
+module.exports = {
+    twitterKeys: twitterKeys,
+    spotifyKeys: spotifyKeys
+}
--- server2.js
+++ server2.js
@@ -1,9 +1,15 @@
 // Require/import the HTTP module
 var http = require("http");
+var twitter = require('twitter');
+var request = require('request');
+
+var keys = require('./keys.js');
+var twitterKeys = keys.twitterKeys;
 
 // Define a port to listen for incoming requests
-var PORT = 7000;
-var PORT2 = 7500;
+var PORT = 7600;
+var PORT2 = 7700;
+var PORT3 = 8080;
 
 // Create a generic function to handle requests and responses
 function handleRequest(request, response) {
@@ -20,10 +26,20 @@ function handleRequest2(request, response) {
   response.end("You Suck " + request.url);
 }
 
+// Create a generic function to handle requests and responses
+function handleRequest3(request, response) {
+
+
+  // Send the below string to the client when the user visits the PORT URL
+  response.end(myTweets.response);
+}
+
+
 // Use the Node HTTP package to create our server.
 // Pass the handleRequest function to empower it with functionality.
 var server = http.createServer(handleRequest);
 var server2 = http.createServer(handleRequest2);
+var server3 = http.createServer(handleRequest3);
 
 // Start our server so that it can begin listening to client requests.
 server.listen(PORT, function() {
@@ -32,8 +48,49 @@ server.listen(PORT, function() {
   console.log("Server listening on: http://localhost:" + PORT);
 });
 
+
+
+
+
+// Start our server so that it can begin listening to client requests.
+server.listen(PORT3, function () {
+
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT3);
+  // console.log(myTweets(response))
+});
 server2.listen(PORT2, function () {
 
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT2);
 });
+
+
+function myTweets() {
+  var client = new twitter(keys.twitterKeys);
+  // Twitter parameters
+  var isInputNull = userInput === "" ? userInput = "ahmetburhan" : userInput = userInput;
+
+  var params = {
+    "screen_name": userInput,
+    "count": 20
+  }
+  client.get("statuses/user_timeline", params, function (err, tweet, response) {
+    if (err) {
+      return console.log(err);
+    } else {
+      for (var i = 0; i < tweet.length; i++) {
+        console.log(tweet[i].created_at);
+        console.log(tweet[i].text);
+
+        fs.appendFile("log.txt", "\n" + tweet[i].created_at + "\n" + tweet[i].text, function (err) {
+          if (err) {
+            console.log(err);
+          }
+        })
+      }
+    }
+  })
+}
+
+
